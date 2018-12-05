6f706a6071f68d0fb02ad6b0db6afc6b22bc9486
ubuntuvim <1527254027@qq.com>
2018-12-06 02:32:55 +0800

    增加项目说明

--- test/java/com/ubuntuvim/coverage/CoverageTest.java
+++ test/java/com/ubuntuvim/coverage/CoverageTest.java
@@ -7,9 +7,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import com.ubuntuvim.coverage.Coverage;
-import com.ubuntuvim.coverage.MyException;
-
 public class CoverageTest {
 
 	@Rule
@@ -22,37 +19,39 @@ public class CoverageTest {
 		c = new Coverage();
 	}
 	
-	@Test
+	@Test  // 测试type为1的情况
 	public void testNormalOne() {
 		String s = c.normal(1);
 		assertEquals("one", s);
 	}
 
-	@Test
+	@Test  // 测试type为2的情况
 	public void testNormalOther() {
 		String s = c.normal(2);
 		assertEquals("other", s);
 	}
 
-	@Test
+	@Test  // 测试正常的情况 （try语句块里的代码）
 	public void testExceptMethodP() {
 		int i = c.exceptMethod(10);
 		assertEquals(2, i);
 	}
 	
+	// 测试出现异常的情况（catch语句块里的代码）
 	@Test(expected = ArithmeticException.class)
 	public void testExceptMethod() {
 		c.exceptMethod(0);
 	}
 
-	@Test
+	@Test  // 测试正常的情况
 	public void testThrowExceptP() throws MyException {
 		int i = c.throwExcept(10);
 		assertEquals(2, i);
 	}
 	
-	@Test
+	@Test  // 测试出现异常的情况
 	public void testThrowExcept() throws MyException {
+		// 出现异常的情况会返回MyException异常
 		thrown.expect(MyException.class);
 		thrown.expectMessage("0不能做分母。");
 		c.throwExcept(0);
